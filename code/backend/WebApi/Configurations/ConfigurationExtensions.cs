d17d74ff9fce68e96a1a06aac866c7fd2b622765
Valerycadovic <valery_chadovich@epam.com>
2020-05-17 23:24:37 +0300

    move backend to specified folder

---
+++ code/backend/WebApi/Configurations/ConfigurationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using DbUp;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApi.Configurations
+{
+    public static class ConfigurationExtensions
+    {
+        public static void AddDbUp(this IConfiguration self)
+        {
+            var connectionString = self.GetConnectionString("DefaultConnection");
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader = DeployChanges.To.SqlDatabase(connectionString, null)
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                .WithTransaction()
+                .Build();
+
+            if (upgrader.IsUpgradeRequired())
+            {
+                upgrader.PerformUpgrade();
+            }
+        }
+    }
+}
---
+++ code/backend/WebApi/SqlScripts/03-StoredProcedures-News_GetDetailed.sql
@@ -0,0 +1,8 @@
+﻿create procedure dbo.News_GetDetailed (@NewsId uniqueidentifier) as
+begin
+	set nocount on
+
+	select n.Id, n.Image, n.Header, n.DetailedViewContent
+	from dbo.News n
+	where n.Id = @NewsId
+end
